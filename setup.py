0c0596faa2affbcfd7cdb59f990d7087d543a2e4
johnchase <chasejohnh@gmail.com>
2018-05-11 15:24:09 -0700

    Added initial framework

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='q2-phylofactor',
+    version='0.0.0',
+    license='BSD-3-Clause',
+    packages=find_packages(),
+    author='John Chase',
+    author_email='chasejohnh@gmail.com',
+    url='https://github.com/johnchase/q2-phylofactor',
+    entry_points={
+        'qiime2.plugins':
+        ['q2-phylofactor=q2_phylofactor.plugin_setup:plugin']
+    },
+    zip_safe=False,
+)
