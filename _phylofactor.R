b3b7796d72ef0dba708cb226f8eb8572b1bb0102
Alex Washburne <bigalculus@gmail.com>
2018-05-14 18:53:53 -0400

    phylofactor R script
    loads q2 input & runs phylofactor

---
+++ _phylofactor.R
@@ -0,0 +1,12 @@
+Data <- read.csv('')
+tree <- read.tree('')
+MetaData <- read.csv('')
+taxonomy <- read.csv('')
+formula <- read.csv('')
+response.variable.class <- read.csv('')
+formula <- as.formula(formula)
+nfactors <- read.csv('')
+ncores <- read.csv('')
+
+pf <- PhyloFactor(Data,tree,MetaData,formula,choice='F',nfactors=nfactors,ncores=ncores)
+  
