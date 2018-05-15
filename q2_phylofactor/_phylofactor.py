7f224554f3e91c8a6e4eaa2bc491ac1afc285e4c
johnchase <chasejohnh@gmail.com>
2018-05-15 13:41:08 -0700

    updated python functions

--- q2_phylofactor/_phylofactor.py
+++ q2_phylofactor/_phylofactor.py
@@ -1,5 +1,74 @@
-from q2_types.feature_table import FeatureTable, Frequency
+import os
+import subprocess
+import tempfile
+import biom
 
-def phylofactor(text: str) -> FeatureTable[Frequency]:
 
-    pass
+from q2_types.feature_table import FeatureTable, Frequency, BIOMV210Format
+from q2_types.tree import NewickFormat
+from q2_types.feature_data import TSVTaxonomyFormat
+
+from qiime2 import Metadata
+
+def run_commands(cmds, verbose=True):
+    if verbose:
+        print("Running external command line application(s). This may print "
+              "messages to stdout and/or stderr.")
+        print("The command(s) being run are below. These commands cannot "
+              "be manually re-run as they will depend on temporary files that "
+              "no longer exist.")
+    for cmd in cmds:
+        if verbose:
+            print("\nCommand:", end=' ')
+            print(" ".join(cmd), end='\n\n')
+        subprocess.run(cmd, check=True)
+
+def _phylofactor(table,
+                 phylogeny,
+                 taxonomy,
+                 metadata,
+                 formula,
+                 choice,
+                 nfactors,
+                 ncores):
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        biom_fp = os.path.join(temp_dir_name, 'output.tsv.biom')
+        cmd = ['_phylofactor.R',
+               str(table),
+               biom_fp,
+               str(phylogeny),
+               str(taxonomy),
+               str(metadata),
+               str(formula),
+               str(choice),
+               str(nfactors),
+               str(ncores)]
+        try:
+            run_commands([cmd])
+        except subprocess.CalledProcessError as e:
+            raise Exception("An error was encountered with PhyloFactor"
+                            " in R (return code %d), please inspect stdout"
+                            " and stderr to learn more." % e.returncode)
+    with open(biom_fp) as fh:
+        table = biom.Table.from_tsv(fh)
+    return table
+
+
+def phylofactor(table: BIOMV210Format,
+                phylogeny: NewickFormat,
+                taxonomy: TSVTaxonomyFormat,
+                metadata: Metadata,
+                formula: str,
+                choice: str='F',
+                nfactors: int=10,
+                ncores: int=1
+                 ) -> (biom.Table):
+    return _phylofactor(table,
+                        phylogeny,
+                        taxonomy,
+                        metadata,
+                        formula,
+                        choice,
+                        nfactors,
+                        ncores,
+                        )
--- q2_phylofactor/assets/_phylofactor.R
+++ q2_phylofactor/assets/_phylofactor.R
@@ -1,18 +1,38 @@
-Data <- read.csv('')
-tree <- read.tree('')
-MetaData <- read.csv('')
-taxonomy <- read.csv('')
+args <- commandArgs(TRUE)
 
-formula <- as.formula(formula)
-
-# LHS <- as.character(formula[[2]]) ## will need to check LHS and family.
-
-pf <- PhyloFactor(Data,tree,MetaData,formula,choice='F',nfactors=nfactors,ncores=ncores)
-  
-## semantic type 1 - artifact (table)
-write.csv(pf$factors,sep='\t',row.names = TRUE,col.names = TRUE)
+Data <- args[[1]]
+tree <- args[[2]]
+out.path <- args[[3]]
+metadata <- args[[4]]
+taxonomy <- args[[5]]
+formula <- as.formula(args[[6]])
+choice <- args[[7]]
+nfactors <- as.integer(args[[8]])
+ncores <- as.integer(args[[9]])
 
 
-tr <- pf.tree(pf,factors=factors)
-##semantic type 2 - visualization
-tr$ggplot
+# Data <- read.csv('')
+# tree <- read.tree('')
+# MetaData <- read.csv('')
+# taxonomy <- read.csv('')
+#
+# formula <- as.formula(formula)
+#
+# # LHS <- as.character(formula[[2]]) ## will need to check LHS and family.
+#
+# pf <- PhyloFactor(
+#   Data,
+#   tree,
+#   MetaData,
+#   formula,
+#   choice,
+#   nfactors,
+#   ncores)
+#
+# ## semantic type 1 - artifact (table)
+# write.csv(pf$factors, sep='\t', row.names=TRUE, col.names=TRUE)
+#
+#
+# tr <- pf.tree(pf,factors=factors)
+# ##semantic type 2 - visualization
+# tr$ggplot
