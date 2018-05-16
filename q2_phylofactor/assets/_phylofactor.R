f07be4472cb2483bd5d33c3e0be36fc172cbc34c
johnchase <chasejohnh@gmail.com>
2018-05-15 17:37:58 -0700

    Place holder files

--- q2_phylofactor/assets/_phylofactor.R
+++ q2_phylofactor/assets/_phylofactor.R
@@ -1,17 +1,26 @@
+#!/usr/bin/env Rscript
+
+# cat(R.version$version.string, "\n")
+R.version
 args <- commandArgs(TRUE)
-
-Data <- args[[1]]
-tree <- args[[2]]
-out.path <- args[[3]]
-metadata <- args[[4]]
-taxonomy <- args[[5]]
-formula <- as.formula(args[[6]])
-choice <- args[[7]]
-nfactors <- as.integer(args[[8]])
-ncores <- as.integer(args[[9]])
-
-
-# Data <- read.csv('')
+# # print(args)
+# table.path <- args[[1]]
+# # library(MASS)
+# # write.table(motors, file=table.path, sep='\t', row.names=TRUE, col.names=TRUE)
+#
+#
+# # tree.path <- args[[2]]
+# # out.path <- args[[3]]
+# # metadata.path <- args[[4]]
+# # taxonomy.path <- args[[5]]
+# # formula <- as.formula(args[[6]])
+# # choice <- args[[7]]
+# # nfactors <- as.integer(args[[8]])
+# # ncores <- as.integer(args[[9]])
+# print('***********************ARE we getting here!!!!!!!*******')
+# print(table.path)
+# # Data <- read.csv(table.path)
+# # write.csv(pf$factors, file = out.path, sep = '\t', row.names = TRUE, col.names = TRUE)
 # tree <- read.tree('')
 # MetaData <- read.csv('')
 # taxonomy <- read.csv('')
