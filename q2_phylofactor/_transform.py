08a2c54dd8b2dd3a3779266bbf8b28f6ae2cdd2b
johnchase <chasejohnh@gmail.com>
2018-06-14 11:57:23 -0700

    current updates

--- q2_phylofactor/_transform.py
+++ q2_phylofactor/_transform.py
@@ -1,64 +1,29 @@
 import qiime2
 import pandas as pd
 
-from q2_phylofactor.import FactorsFormat
+from q2_phylofactor import FactorsFormat
 from q2_phylofactor.plugin_setup import plugin
-
-
-def _factor_group_to_dataframe(filepath):
-    """Convert FactorFormat to pandas dataframe
-    ----------
-    filepath : str
-        Filepath pointing to the FactorFormat to transform.
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame containing the Factors data. The index will be
-    """
-
-    df = pd.read_csv(filepath, sep='\t')
-
-    if len(set(['factor', 'featureid', 'group']) - set(df.columns)) != 0:
-        raise ValueError(
-            "Factor format requires a header with `factor`, `group' and "
-            "`featureid`")
-
-    if len(df.index) < 1:
-        raise ValueError("Taxonomy format requires at least one row of data.")
-
-    return df
-
-
-def _dataframe_to_factor_group(df):
-    if len(df.index) < 1:
-        raise ValueError("Factor format requires at least one row of data.")
-
-    if len(set(['factor', 'featureid', 'group']) - set(df.columns)) != 0:
-        raise ValueError(
-            "Factor format requires a header with `factor`, `group' and "
-            "`featureid`")
-
-    ff = FactorsFormat()
-    df.to_csv(str(ff), sep='\t', header=True, index=True)
-    return ff
 
 
 @plugin.register_transformer
 def _1(ff: FactorsFormat) -> pd.DataFrame:
-    return _factor_group_to_dataframe(str(ff))
+    return pd.read_csv((str(ff)), sep='\t')
 
 
 @plugin.register_transformer
 def _2(df: pd.DataFrame) -> FactorsFormat:
-    return _dataframe_to_factor_group(df))
-
-@plugin.register_transformer
-def _3(ff: FactorsFormat) -> qiime2.Metadata:
-    return qiime2.Metadata.load(str(ff))
-
-
-@plugin.register_transformer
-def _4(obj: qiime2.Metadata) -> FactorsFormat:
     ff = FactorsFormat()
-    obj.save(str(ff))
+    df.to_csv(str(ff), sep='\t', header=True, index=True)
     return ff
+
+# These do not currently work
+# @plugin.register_transformer
+# def _3(ff: FactorsFormat) -> qiime2.Metadata:
+#     return qiime2.Metadata.load(str(ff))
+#
+#
+# @plugin.register_transformer
+# def _4(obj: qiime2.Metadata) -> FactorsFormat:
+#     ff = FactorsFormat()
+#     obj.save(str(ff))
+#     return ff
